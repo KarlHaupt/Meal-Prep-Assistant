cb0612b256997d528c8cd5009836b9f6e9a09ecb
Gabriel da Silva <gabrield@bbd.co.za>
2023-05-10 14:29:12 +0200

    Created preference db insert script

---
+++ DBScripts/Insert DB script.sql
@@ -0,0 +1,14 @@
+USE MealPlanDB;
+GO
+
+INSERT INTO [dbo].[lookup_preferences] (preference_type) VALUES 
+('Vegetarian'),
+('Halaal'),
+('Vegan'),
+('Dairy-free'),
+('Gluten-free'),
+('Low-carb'),
+('High-protein'),
+('Quick & easy'),
+('Budget-friendly'),
+('Explore new flavours');
