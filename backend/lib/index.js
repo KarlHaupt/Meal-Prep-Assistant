d33a7faa43742e24b54548b5c2d07bec5be62f8a
Janco Venter <jan@bbd.co.za>
2023-05-10 14:55:07 +0200

    Simple wrapper to generate meal plan and get recipes

---
+++ backend/lib/index.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+module.exports = class MealPlan {
+    constructor(apiKey) {
+        this.baseUrl = 'https://api.spoonacular.com';
+        this.apiKey = apiKey;
+    }
+
+    async generateMealPlan(targetCalories=2000, diet='', exclude='') {
+        let timeFrame = 'day'
+        let url = `${this.baseUrl}/mealplanner/generate?apiKey=${this.apiKey}&targetCalories=${targetCalories}&timeFrame=${timeFrame}`;
+        if (diet){
+            url += `&${diet}`;
+        }
+        if (exclude){
+            url += `&${exclude}`;
+        }
+
+        const { data } = await axios.get(url);
+        return data;
+    }
+
+    async getRecipe(id) {
+        const { data } = await axios.get(`${this.baseUrl}/recipes/${id}/information?apiKey=${this.apiKey}`)
+        return(data)
+    }
+};
