e968f57d8af741d25c6ed724f54d2cb5188f3bbf
Gabriel da Silva <gabrield@bbd.co.za>
2023-05-16 16:19:27 +0200

    Added to preferences controller to call stored proc

---
+++ backend/controllers/preferencesController.js
@@ -0,0 +1,39 @@
+const Request = require('tedious').Request;
+const { connection } = require('../config/connectDatabase');
+const ErrorHandler = require('../utils/ErrorHandler');
+const catchAsyncError = require('../middlewares/catchAsyncError');
+const { TYPES } = require('tedious');
+
+//Get user preferences
+exports.getPreferences = catchAsyncError(async (req, res, next) => {
+    const { email } = req.body;
+    
+    let sql = "spPreferences_GetByUserEmail"
+    
+    const request = new Request(sql, function(err) {
+        if(err) {
+            return next(new ErrorHandler('Internal Server Error', 500));
+        } 
+    });
+
+    request.addParameter('email', TYPES.NVarChar, email);
+
+    connection.callProcedure(request);
+
+    request.on('requestCompleted', function(rowCount, rows) {
+        const preferencesReceived = false;
+        if(rowCount >= 1) {
+            const preferencesReceived = true;
+        }
+
+        if(!preferencesReceived) {
+            return next(new ErrorHandler('Unable to obtain preferences', 401));
+        }
+        
+        res.status(200).json({
+            success: true,
+            message: 'Preferences obtained',
+            user_preferences: rows
+        });
+    });
+});
