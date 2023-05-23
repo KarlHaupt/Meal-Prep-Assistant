29165050ee84b3196569e70f130e07282dfd4440
Janco Venter <jan@bbd.co.za>
2023-05-23 16:29:28 +0200

    Add some logging tools

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const errorMiddleware = require('./middlewares/error');
 const { loginView } = require('./controllers/userController');
 
+app.use(morgan('tiny'));
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
