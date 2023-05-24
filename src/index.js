6f6bb2522a86074089b5a67829188942013d7e6a
Marni Kleingeld <marni@bbd.co.za>
2023-05-24 15:19:17 +0200

    serve root page in index

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
-const app = require('./app');
-const { connectToDatabase } = require('./config/connectDatabase');
+const app = require("./app");
+const { connectToDatabase } = require("./config/connectDatabase");
+const { loginView } = require("./controllers/userController");
 
-process.on('uncaughtException', err => {
-    console.log(`ERROR: ${err}`);
-    console.log('Shutting down due to uncaught exception');
-    process.exit(1);
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err}`);
+  console.log("Shutting down due to uncaught exception");
+  process.exit(1);
 });
 
 connectToDatabase();
 
 const server = app.listen(8080, () => {
-    console.log(`Server started on PORT: 8080 in ${process.env.NODE_ENV}`);
+  console.log(`Server started on PORT: 8080 in ${process.env.NODE_ENV}`);
 });
 
-process.on('unhandledRejection', err => {
-    console.log(`ERROR: ${err}`);
-    console.log('Shutting down the server due to unhandled Promise Rejection');
-    server.close(() => process.exit(1));
+app.get("/", loginView);
+
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err}`);
+  console.log("Shutting down the server due to unhandled Promise Rejection");
+  server.close(() => process.exit(1));
 });
